d0eebb0ff1a87c799e9e9ae260224382b306fe07
abdulafeel <abdulafeel2@gmail.com>
2022-09-04 12:52:40 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+require("./models/User");
+require("./models/Track");
+const express = require('express');
+const mongoose= require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes=require('./routes/authRoutes');
+const trackRoutes=require('./routes/trackRoutes');
+const requireAuth = require("./middlewares/requireAuth");
+
+const app=express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+const mongoUri="mongodb+srv://admin:passwordpassword@cluster0.bbaqbuf.mongodb.net/?retryWrites=true&w=majority";
+if(!mongoUri){
+    throw new Error(
+        'MongUri was not supplied'
+    );
+}
+mongoose.connect(mongoUri);
+mongoose.connection.on("connected",()=>{
+    console.log("connected to mongo instance");
+});
+mongoose.connection.on("error",(err)=>{
+    console.error("error connected to mongo instance",err);
+});
+
+app.get('/',requireAuth,(req,res)=>{
+    res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000");
+});
